292ec58ac7886b9cc592082950f99e152290d351
Jeff Byrnes <jeff@evertrue.com>
2013-12-19 11:57:39 -0500

    Bump to v1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 1.2.0:
+
+* Use newrelic-ng instead of newrelic
+* Optimize to only make one `Chef::EncryptedDataBagItem.load()` call
+* Pin to Berkshelf ~> 2.0.10
+* Use generic email address
+* Flesh out README
+
 ## 1.1.0:
 
 * Add Datadog
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.2.0'
 
 depends 'copperegg'
 depends 'newrelic-ng', '~> 0.4.0'
