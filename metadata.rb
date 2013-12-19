a99fa448e50ebacbcb5e019df366959f0a55a324
Jeff Byrnes <jeff@evertrue.com>
2013-12-19 11:55:48 -0500

    Use newrelic-ng, optimize data bag loading

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
 
 depends 'copperegg'
-depends 'newrelic'
+depends 'newrelic-ng', '~> 0.4.0'
 depends 'et_datadog', '= 1.0.1'
--- recipes/agent.rb
+++ recipes/agent.rb
@@ -7,12 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.set['newrelic']['server_monitoring']['license'] = Chef::EncryptedDataBagItem.load('secrets', 'api_keys')['newrelic']
-node.set['copperegg']['apikey'] = Chef::EncryptedDataBagItem.load('secrets', 'api_keys')['copperegg']
+api_keys = Chef::EncryptedDataBagItem.load('secrets', 'api_keys')
+
+node.set['newrelic-ng']['license_key'] = api_keys['newrelic']
+node.set['copperegg']['apikey'] = api_keys['copperegg']
 
 include_recipe 'copperegg'
 
-include_recipe 'newrelic::repository'
-include_recipe 'newrelic::server-monitor'
+include_recipe 'newrelic-ng'
 
 include_recipe 'et_datadog'
