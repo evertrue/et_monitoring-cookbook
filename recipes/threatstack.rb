947d31553534cb539871e7967ba0beb6ad5b4389
Jeff Byrnes <thejeffbyrnes@gmail.com>
2015-12-16 17:10:26 -0500

    Fix setting of node attribute in recipe

--- recipes/threatstack.rb
+++ recipes/threatstack.rb
@@ -4,6 +4,6 @@
 #
 # Copyright (c) 2015 EverTrue, All Rights Reserved.
 
-set['threatstack']['deploy_key'] = data_bag_item('secrets', 'api_keys')['threatstack']
+node.set['threatstack']['deploy_key'] = data_bag_item('secrets', 'api_keys')['threatstack']
 
 include_recipe 'threatstack'
