37f9c895184f1839bd1bcad8065ab0aed380534f
Eric Herot <eric.github@herot.com>
2016-04-06 16:53:18 -0400

    Withdrawing some hugs from teh test codes

--- test/integration/agent/serverspec/snmpd_spec.rb
+++ test/integration/agent/serverspec/snmpd_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'et_monitoring::snmpd' do
-  describe service('snmpd') do
+  describe service 'snmpd' do
     it { is_expected.to be_enabled }
     it { is_expected.to be_running }
   end
 
-  describe command('snmpwalk -v 1 -c testcommunity localhost .1.3.6.1.4.1.2021.9.1.10') do
-    its(:stdout) { is_expected.to include('iso.3.6.1.4.1.2021.9.1.10.1 = INTEGER: 12') }
+  describe command 'snmpwalk -v 1 -c testcommunity localhost .1.3.6.1.4.1.2021.9.1.10' do
+    its(:stdout) { is_expected.to include 'iso.3.6.1.4.1.2021.9.1.10.1 = INTEGER: 12' }
   end
 end
