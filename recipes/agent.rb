827fb33c0c5414046909e50a3fd634045d980b60
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-05-04 09:52:41 -0400

    Ensure NR Server Agent can monitor Docker
    This allows the New Relic Server Agent permission to monitor Docker
    containers, which will provide valuable insight into some of our
    Mesos-backed infra.

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -12,3 +12,15 @@ node.set['newrelic']['license'] = data_bag_item('secrets', 'api_keys')['newrelic
 include_recipe 'newrelic'
 include_recipe 'et_monitoring::threatstack' if node['et_monitoring']['threatstack_enabled']
 include_recipe 'et_monitoring::snmpd'
+
+# Ensure New Relic can monitor Docker containers, if it is installed
+service 'newrelic-sysmond' do
+  action :nothing
+end
+
+group 'docker' do
+  members %w(docker newrelic)
+  action :modify
+  only_if { node['etc']['passwd']['docker'] }
+  notifies :restart, 'service[newrelic-sysmond]'
+end
