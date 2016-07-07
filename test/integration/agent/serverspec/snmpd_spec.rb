138f66661319580198b4413e0b36fde780a90dce
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-07-07 14:25:15 -0400

    Update SNMP assertion
    Update assertion to match any non-zero integer

--- test/integration/agent/serverspec/snmpd_spec.rb
+++ test/integration/agent/serverspec/snmpd_spec.rb
@@ -6,7 +6,9 @@ describe 'et_monitoring::snmpd' do
     it { is_expected.to be_running }
   end
 
+  # As per https://github.com/evertrue/et_monitoring-cookbook/pull/5#issuecomment-231171948
+  # this test now looks for a line that has a non-zero integer
   describe command 'snmpwalk -v 1 -c testcommunity localhost .1.3.6.1.4.1.2021.9.1.10' do
-    its(:stdout) { is_expected.to include 'iso.3.6.1.4.1.2021.9.1.10.1 = INTEGER: 12' }
+    its(:stdout) { is_expected.to match(/^iso.3.6.1.4.1.2021.9.1.10.1 = INTEGER: [1-9]\d*$/) }
   end
 end
