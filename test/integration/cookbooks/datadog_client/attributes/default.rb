98f11eb7baa8d2544b7abc23d749f44533db25fb
Eric Herot <eric.github@herot.com>
2016-09-07 17:52:00 -0400

    Switch to using a test cookbook to set test datadog attributes to more accurately reflect how wrappers do it

---
+++ test/integration/cookbooks/datadog_client/attributes/default.rb
@@ -0,0 +1,13 @@
+if node.chef_environment == 'dev'
+  default['et_monitoring']['datadog']['test_service'] = {
+    instances: [{
+      host: 'localhost',
+      port: 22,
+      timeout: 8,
+      tags: {
+        test_service: node.chef_environment,
+        env: node.chef_environment
+      }
+    }]
+  }
+end
