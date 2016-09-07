c241b8f9a7bdec68574947d14fb64f07bb195829
Eric Herot <eric.github@herot.com>
2016-09-07 17:14:06 -0400

    Set use_integration_template to true if cookbook is not defined
    This way we don't have to either set this option everywhere or define a totally redundant template erb that's exactly the same as the one that's already in the datadog cookbook.

--- recipes/_datadog.rb
+++ recipes/_datadog.rb
@@ -15,5 +15,13 @@ include_recipe 'datadog::dd-handler'
 node['et_monitoring']['datadog'].each do |dd_monitor, conf|
   datadog_monitor dd_monitor do
     conf.each { |key, value| send(key, value) }
+
+    # This is a little confusing...
+    #
+    # If a cookbook is defined, we expect to use the template from that
+    # cookbook. Otherwise, use the integration.yaml.erb included as part of the
+    # Datadog cookbook:
+    # https://github.com/DataDog/chef-datadog/blob/master/templates/default/integration.yaml.erb
+    use_integration_template !conf['cookbook']
   end
 end
