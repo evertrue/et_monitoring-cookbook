df8e1527b693869d5db49ec0fdb3333f1ede3d31
Eric Herot <eric.github@herot.com>
2017-02-24 16:32:22 -0500

    Version bump 3.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.1.1'
+version          '3.1.2'
 
 depends 'newrelic',   '~> 2.0'
 depends 'et_cloudwatch', '~> 1.0'
