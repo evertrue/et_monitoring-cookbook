ca83d5f8f87e10d533f19f335d6a680ca938977f
Eric Herot <eric.github@herot.com>
2017-07-11 16:09:09 -0400

    Automatically detect the device number for / when testing snmp inode detection

--- test/integration/default/serverspec/snmpd_spec.rb
+++ test/integration/default/serverspec/snmpd_spec.rb
@@ -6,9 +6,15 @@ describe 'et_monitoring::snmpd' do
     it { is_expected.to be_running }
   end
 
+  # Gets the actual SNMP device number for the '/' device
+  o = `snmpwalk -v 1 -c testcommunity localhost .1.3.6.1.4.1.2021.9.1.2`
+  snmp_device_number = o.split("\n").grep(%r{"/"})[0].split[0].split('.')[-1]
+
   # As per https://github.com/evertrue/et_monitoring-cookbook/pull/5#issuecomment-231171948
   # this test now looks for a line that has a non-zero integer
-  describe command 'snmpwalk -v 1 -c testcommunity localhost .1.3.6.1.4.1.2021.9.1.10' do
-    its(:stdout) { is_expected.to match(/^iso.3.6.1.4.1.2021.9.1.10.1 = INTEGER: [1-9]\d*$/) }
+  describe command "snmpwalk -v 1 -c testcommunity localhost .1.3.6.1.4.1.2021.9.1.10.#{snmp_device_number}" do
+    its(:stdout) do
+      is_expected.to match(/iso.3.6.1.4.1.2021.9.1.10.#{snmp_device_number} = INTEGER: [1-9]/)
+    end
   end
 end
