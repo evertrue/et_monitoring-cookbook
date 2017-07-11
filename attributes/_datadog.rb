12bab0d0ee01503fd727cc9ffdb0f2627aba1f9a
Eric Herot <eric.github@herot.com>
2017-07-11 14:15:05 -0400

    Install different datadog package version on 16.04

--- attributes/_datadog.rb
+++ attributes/_datadog.rb
@@ -1,2 +1,5 @@
-default['datadog']['agent_version'] = '1:5.10.1-1'
+default['datadog']['agent_version'] = value_for_platform ubuntu: {
+  %w(12.04 14.04) => '1:5.10.1-1',
+  '16.04' => '1:5.14.1-1'
+}
 default['et_monitoring']['datadog'] = {}
